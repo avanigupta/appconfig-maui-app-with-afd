34c51e2de342dc135953eb46c8586ddf4e3ad6d1
Avani Gupta <avgupta@microsoft.com>
2025-11-17 17:16:23 -0800

    Sample code

---
+++ MauiAppConfigDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiAppConfigDemo;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ MauiAppConfigDemo/MauiProgram.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace MauiAppConfigDemo;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        var afdEndpoint = new Uri("https://YOUR-AFD-ENDPOINT.azurefd.net");
+
+        builder.Configuration.AddAzureAppConfiguration(options =>
+        {
+            options.ConnectAzureFrontDoor(afdEndpoint)
+                .SelectSnapshot("TravelAppSnapshot")
+                .Select("TravelApp:*")
+                .UseFeatureFlags(featureFlagOptions =>
+                {
+                    featureFlagOptions.Select("TravelApp.*")
+                        .SetRefreshInterval(TimeSpan.FromMinutes(1));
+                })
+                .ConfigureRefresh(refreshOptions =>
+                {
+                    refreshOptions.RegisterAll()
+                        .SetRefreshInterval(TimeSpan.FromMinutes(1));
+                });
+        });
+
+        builder.Services.AddAzureAppConfiguration();
+        builder.Services.AddSingleton<ConfigurationService>();
+        builder.Services.AddFeatureManagement();
+        builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
